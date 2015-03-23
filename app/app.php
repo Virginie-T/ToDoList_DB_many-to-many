ddcca27cb6b5c2e96228914e24c39a4c501c118d
Conor Baumgart & Virginie Trubiano <student@epicodus.com>
2015-03-23 14:17:13 -0700

    add more path in app and views pages

---
+++ app/app.php
@@ -0,0 +1,56 @@
+<?php
+
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
+
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    $DB = new PDO('pgsql:host=localhost;dbname=to_do');
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__."/../views"
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.twig', array('categories' => Category::getAll(), 'tasks' => Task::getAll()));
+    });
+
+    $app->get("/tasks", function() use ($app) {
+        return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
+    });
+
+    $app->get("/categories", function() use ($app) {
+        return $app['twig']->render('categories.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/tasks", function() use ($app) {
+        $description = $_POST['description'];
+        $task = new Task($description);
+        $task->save();
+        return $app['twig']->render('task.twig', array( 'task' => Task::getAll()));
+    });
+
+    $app->get("/tasks/{id}", function($id) use ($app) {
+        $task = Task::find($id);
+        return $app['twig']->render('task.twig', array('task' =>$task, 'categories' => $task->getCategories(), 'all_categories' => Category::getAll()));
+    });
+
+    $app->post("/categories", function() use ($app) {
+        $category = new Category($_POST['name']);
+        $category->save();
+        return $app['twig']->render('categories.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->get("/categories/{id}", function($id) use ($app) {
+        $category = Category::find($id);
+        return $app['twig']->render('categories.twig', array('categories' => $category, 'tasks' => $category->getTasks(), 'all_tasks' => Task::getAll()));
+    });
+
+    return $app;
+
+?>
